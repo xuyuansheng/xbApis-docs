6e653f25cbca2276c9a0dfb6b3b8d25cbfe0411a
浮躁的菜 <804288658@qq.com>
2019-07-21 07:01:25 +0800

    初次提交

---
+++ src/main/java/cn/xuxiaobu/doc/xbapidocs/XbapiDocsApplication.java
@@ -0,0 +1,13 @@
+package cn.xuxiaobu.doc.xbapidocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XbapiDocsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XbapiDocsApplication.class, args);
+    }
+
+}
